84b18a4e644d64b5aed7b0111174c5072065c641
coolistkovbel <coolistkovbel@gmail.com>
2022-06-21 03:39:13 -0400

    slow start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>deHub</h1>
+        <p>A place to be to spill your thoughts onto the blockchain</p>
       </header>
+      <section>
+        <h2>De Hub</h2>
+      </section>
     </div>
   );
 }
